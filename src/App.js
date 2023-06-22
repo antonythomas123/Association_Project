3210489a7f05dbb1a8a0e1bb2c930d0f97e8f15c
Antony Thomas <antonythomas993@gmail.com>
2023-06-22 17:25:55 +0530

    Lazy Loading added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home/Home";
-import DetailsPage from "./components/DetailsPage/DetailsPage";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import { createContext } from "react";
+import { createContext, lazy, Suspense } from "react";
 import events from "./data/EventData/EventData";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 export const AppContext = createContext();
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const DetailsPage = lazy(() => import("./components/DetailsPage/DetailsPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 
 function App() {
   return (
     <AppContext.Provider value={events}>
       <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/details/:title" element={<DetailsPage />} />
-            <Route path="registerpage" element={<RegisterPage/>}/>
-          </Routes>
-          <Footer />
-        </div>
+        <Suspense fallback={<h1>Loading.....!</h1>}>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/details/:title" element={<DetailsPage />} />
+              <Route path="registerpage" element={<RegisterPage />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Suspense>
       </BrowserRouter>
     </AppContext.Provider>
   );
